a35f4d0fa066f97954ef2f217df5d0f433aaf52e
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-09-29 21:17:53 +0530

    Learned about count loop

---
+++ 4-count-loop/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "sg_allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow port 22 for ssh access"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = var.ssh_port
+    to_port          = var.ssh_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "Terraform allow_ssh"
+  }
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  count                  = 3
+  ami                    = var.ami_devops
+  instance_type          = var.environment == "PROD" ? "t3.small" : "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg_allow_ssh.id]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
---
+++ 4-count-loop/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_devops" {
+  type        = string
+  description = "This is the ami id of devops-practice"
+  default     = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "environment" {
+  type    = string
+  default = "DEV"
+}
+
+variable "ssh_port" {
+  type    = number
+  default = 22
+}
