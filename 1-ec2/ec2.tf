341c68acc6e556e4b2af5b329f7f5e7ecd6224b6
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-09-29 19:18:22 +0530

    Updated resource

--- 1-ec2/ec2.tf
+++ 1-ec2/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_security_group" "sg_allow_ssh" {
 	name	= "allow_ssh"
+	description = "Allow port 22 for ssh access"
 	egress {
 		from_port	= 0
 		to_port		= 0
@@ -13,5 +14,8 @@ resource "aws_security_group" "sg_allow_ssh" {
 		protocol	= "tcp"
 		cidr_blocks	= ["0.0.0.0/0"]
 		ipv6_cidr_blocks= ["::/0"]
+	}
+	tags = {
+		Name = "terraform allow_ssh"
 	}
 }
