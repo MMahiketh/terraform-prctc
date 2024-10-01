778866cf6649e758ea51960a42976332cc6bcd59
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-01 20:21:32 +0530

    Created expense project using conditions

--- 7-expense/ec2.tf
+++ 7-expense/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "expense" {
   count                  = length(var.servers)
   ami                    = data.aws_ami.devops-practice_ami.id
-  instance_type          = 
+  instance_type          = var.servers[count.index] == "mysql" ? "t3.small" : "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = merge(
     var.common_tags, { Name = var.servers[count.index] }
